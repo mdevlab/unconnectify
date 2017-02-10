7fa235074d3d5cd86108b61954ea2234388156ed
bachiri Taoufiq Abderrahman <bachiri.abderrahman@gmail.com>
2017-02-10 13:03:40 +0000

    check github

--- app/src/main/java/io/mdevlab/unconnectify/MainActivity.java
+++ app/src/main/java/io/mdevlab/unconnectify/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
+
             }
         });
     }
