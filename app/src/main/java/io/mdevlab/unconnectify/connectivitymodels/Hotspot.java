a0f2060aadafe0e342175d102b760acd0d6a48b9
bachiri Taoufiq Abderrahman <bachiri.abderrahman@gmail.com>
2017-02-10 18:42:34 +0000

    hotspot enable/disable

---
+++ app/src/main/java/io/mdevlab/unconnectify/connectivitymodels/Hotspot.java
@@ -0,0 +1,60 @@
+package io.mdevlab.unconnectify.connectivitymodels;
+
+import android.content.Context;
+import android.net.wifi.WifiManager;
+
+import cc.mvdan.accesspoint.WifiApControl;
+
+/**
+ * This class handles connecting to and disconnecting from Hotspot
+ * <p>
+ * Hotspot implements the singleton design pattern since only one instance of this class is needed
+ * to manage enabling/disabling of Hotspot
+ * <p>
+ * For WifiApControl we use accesspoint Library  developed by mvdan
+ * Enabling, disabling and configuring of wireless Access Points are all unaccessible in the SDK behind hidden methods in WifiManager .
+ * Reflection is used to get access to those methods.
+ * <p>
+ * <p>
+ * Created by mdevlab on 2/10/17.
+ */
+
+
+public class Hotspot {
+
+
+    private static Hotspot hotspot = null;
+    private static WifiManager wifiManager;
+    private static WifiApControl apControl;
+
+
+    private Hotspot(Context context) {
+        this.wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        apControl = WifiApControl.getInstance(context);
+    }
+
+    public static Hotspot getInstance(Context context) {
+        if (hotspot == null) {
+            hotspot = new Hotspot(context);
+
+        }
+        return hotspot;
+    }
+
+    //TODO Bachiri Make Sure to Verify apControl is'nt empty befor you run below methods
+    public void disable() {
+        apControl.disable();
+    }
+
+
+    public void enable() {
+        // Wifi must be disabled to enable the access point
+        if (wifiManager.isWifiEnabled())
+            wifiManager.setWifiEnabled(false);
+        apControl.enable();
+
+
+    }
+
+
+}
---
+++ app/src/main/java/io/mdevlab/unconnectify/utils/DialogUtils.java
@@ -0,0 +1,38 @@
+package io.mdevlab.unconnectify.utils;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+
+/**
+ * Created by mdevlab on 2/10/17.
+ */
+
+public class DialogUtils {
+
+    /**
+     * this method is used to redirect users to setting activity ,
+     * user will be redirected to this setting activity until he/she provide the write access ,this function is called in devise runing 6.0+
+     * @param context the context is used for lunching the AlertDialog
+     */
+    public  static void showDialog(final Context context){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.System.canWrite(context)) {
+            new AlertDialog.Builder(context)
+                    .setMessage("Allow reading/writing the system settings? Necessary to set up access points.")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                            intent.setData(Uri.parse("package:" + context.getPackageName()));
+                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            context.startActivity(intent);
+                        }
+                    }).show();
+            return;
+        }
+    }
+}
