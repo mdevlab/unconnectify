d51d5d14c92e28dfdc7cb137f99f1e3ba10bd23a
bachiri Taoufiq Abderrahman <bachiri.abderrahman@gmail.com>
2017-02-23 18:48:08 +0000

    change version to beta

--- app/src/main/java/io/mdevlab/unconnectify/MainActivity.java
+++ app/src/main/java/io/mdevlab/unconnectify/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -146,6 +147,8 @@ public class MainActivity extends AppCompatActivity implements TimePickerDialog.
     public TextView getAlarmsCount() {
         return mAlarmsCount;
     }
+
+
 
 
 }
