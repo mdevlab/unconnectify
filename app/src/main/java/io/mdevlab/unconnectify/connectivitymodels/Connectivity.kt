7d4dff2a6a96b44306c219c7d2f8600e7a754040
husaynhakeem@gmail.com <husaynhakeem@gmail.com>
2018-10-06 15:29:45 -0700

    Add Kotlin plugins & dependencies

---
+++ app/src/main/java/io/mdevlab/unconnectify/connectivitymodels/Connectivity.kt
@@ -0,0 +1,10 @@
+package io.mdevlab.unconnectify.connectivitymodels
+
+/**
+ * Created by mdevlab on 2/10/17.
+ */
+abstract class Connectivity {
+
+    protected abstract fun enable()
+    protected abstract fun disable()
+}
